472e511b427efe74e826fa74b467c48f7d8ca9b2
Erick <erick.fares@agriculturaverion.com.br>
2024-03-26 11:46:21 -0300

    Erick [feat]: Finished Component

--- LoginFilialVerionTela/LoginFilialVerionTela/CardButtonControl.axaml.cs
+++ LoginFilialVerionTela/LoginFilialVerionTela/CardButtonControl.axaml.cs
@@ -22,22 +22,47 @@ public class CardButtonControl : Button
             o => o.ButtonName,
             (o, v) => o.ButtonName = v);
 
+    public static readonly DirectProperty<CardButtonControl, double> CustomHeightProperty =
+        AvaloniaProperty.RegisterDirect<CardButtonControl, double>(
+            nameof(CustomHeight),
+            o => o.CustomHeight,
+            (o, v) => o.CustomHeight = v);
+
+    public static readonly DirectProperty<CardButtonControl, double> CustomWidthProperty =
+        AvaloniaProperty.RegisterDirect<CardButtonControl, double>(
+            nameof(CustomWidth),
+            o => o.CustomWidth,
+            (o, v) => o.CustomWidth = v);
+
     public static readonly DirectProperty<CardButtonControl, IBrush> SvgFillProperty =
         AvaloniaProperty.RegisterDirect<CardButtonControl, IBrush>(
             nameof(SvgFill),
             o => o.SvgFill,
             (o, v) => o.SvgFill = v);
 
+    private double _width = 0;
+    private double _height = 58;
     private Geometry _geometry = new PathGeometry();
     private string _buttonName = string.Empty;
     private IBrush _svgFill = new SolidColorBrush();
 
     public CardButtonControl()
     {
-        SvgFill = new SolidColorBrush(new Color(Byte.MaxValue, 0, Byte.MaxValue, 0));
-        
+        SvgFill = new SolidColorBrush(new Color(255,255,255,255));
         AddHandler(PointerPressedEvent, (_, _) => OnPointerPressedCard(), handledEventsToo: true);
         AddHandler(PointerReleasedEvent, (_, _) => OnPointerReleasedCard(), handledEventsToo: true);
+    }
+
+    public double CustomWidth
+    {
+        get => _width;
+        set => SetAndRaise(CustomWidthProperty, ref _width, value);
+    }
+
+    public double CustomHeight
+    {
+        get => _height;
+        set => SetAndRaise(CustomHeightProperty, ref _height, value);
     }
 
     public Geometry Geometry
@@ -60,11 +85,11 @@ public class CardButtonControl : Button
     
     private void OnPointerPressedCard()
     {
-        SvgFill = new SolidColorBrush(new Color(Byte.MaxValue, Byte.MaxValue, 0, 0));
+        SvgFill = new SolidColorBrush(new Color(255,4,4,4));
     }
 
     private void OnPointerReleasedCard()
     {
-        SvgFill = new SolidColorBrush(new Color(Byte.MaxValue, 0, Byte.MaxValue, 0));
+        SvgFill = new SolidColorBrush(new Color(255,255,255,255));
     }
 }
